ca79979c1b6966d5d19fb2a5c9f19c90a7dd7359
edmundogabrielmartinez <mundogabrielmart@gmail.com>
2025-09-24 19:16:30 -0600

    error solucionado

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -9,22 +9,16 @@ class Producto extends Model
 {
     use HasFactory;
 
-    // Nombre de la tabla (opcional, pero lo pongo para mayor claridad)
     protected $table = 'productos';
 
-    // Clave primaria
     protected $primaryKey = 'id';
 
-    // Si la tabla usa incrementos automáticos
     public $incrementing = true;
 
-    // Tipo de la clave primaria
     protected $keyType = 'int';
 
-    // Si tiene timestamps (como created_at y updated_at)
     public $timestamps = true;
 
-    // Columnas que se pueden asignar de forma masiva
     protected $fillable = [
         'nombre',
         'precio',
